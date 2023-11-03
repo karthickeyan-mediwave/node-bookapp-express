0fdf0cfbe04d892eb76552ef10cb4cadd0643568
karthickeyan-mediwave <karthickeyan@mindwaveventures.com>
2023-11-03 16:45:19 +0530

    book post api added

--- app/index.js
+++ app/index.js
@@ -1,10 +1,32 @@
-var express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
-var app = express();
-app.use(express.json());
 
 const config = require("./config");
+
+const express = require("express");
+const morgan = require("morgan");
+
+// const { notfound } = require("./middlewares/notfound.middleware");
+// const { errorHandler } = require("./middlewares/errorhandler.middleware");
+
+// const bookRouter = require("./routes/book.routes");
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+// // book routes
+// app.use("/books", bookRouter);
+
+// // 404 handler
+// app.use(notfound);
+
+// // error handler middleware
+// app.use(errorHandler);
+
+const { v4: uuidv4 } = require("uuid");
+const Joi = require("joi");
 
 let books = [];
 
@@ -20,64 +42,72 @@ app.get("/books/:id", (request, response) => {
   console.log(accountId);
 
   if (!getAccount) {
-    response.status(500).send("trainee not found.");
+    response.status(500).send("books not found.");
   } else {
     response.json(getAccount);
   }
 });
 
 // add
-app.post("/", (req, res) => {
-  books.push({
-    id: req.body.id,
-    name: req.body.name,
-  });
-  return res.json({
-    message: "books added sucesssfully",
-  });
+
+////
+app.post("/books", (req, res, next) => {
+  const id = uuidv4();
+
+  function isValidISBN(isbn) {
+    // length must be 10
+    let n = isbn.length;
+    if (n != 10) return false;
+
+    // Computing weighted sum of
+    // first 9 digits
+    let sum = 0;
+    for (let i = 0; i < 9; i++) {
+      let digit = isbn[i] - "0";
+
+      if (0 > digit || 9 < digit) return false;
+
+      sum += digit * (10 - i);
+    }
+
+    // Checking last digit.
+    let last = isbn[9];
+    if (last != "X" && (last < "0" || last > "9")) return false;
+
+    // If last digit is 'X', add 10
+    // to sum, else add its value.
+    sum += last == "X" ? 10 : last - "0";
+
+    // Return true if weighted sum
+    // of digits is divisible by 11.
+    return sum % 11 == 0;
+  }
+  let jj = {
+    id: id,
+    title: req.body.title,
+    isbn: req.body.isbn,
+  };
+
+  let isbn = req.body.isbn;
+
+  if (isValidISBN(isbn)) {
+    books.push(jj);
+    return res.json({
+      message: "books added sucesssfully",
+    });
+  } else {
+    return res.json({
+      message: "error",
+    });
+  }
 });
 
 // Update
 
-app.put("/books", (request, response) => {
-  const accountname = String(request.query.name);
-  const body = request.body;
-  const account = books.find((account) => account.name === accountname);
-  const index = books.indexOf(account);
-
-  if (!account) {
-    response.status(500).send("Account not found.");
-  } else {
-    const updatedAccount = { ...account, ...body };
-
-    books[index] = updatedAccount;
-
-    response.send(updatedAccount);
-  }
-  return res.json({
-    message: "books update sucesssfully",
-  });
-});
 app.put("/books/:id", (request, response) => {
   const accountId = Number(request.params.id);
   const body = request.body;
   const account = books.find((account) => account.id === accountId);
-  const index = books.indexOf(account);
-
-  if (!account) {
-    response.status(500).send("Account not found.");
-  } else {
-    const updatedAccount = { ...account, ...body };
-
-    books[index] = updatedAccount;
-
-    response.send(updatedAccount);
-  }
-});
-app.put("/books/", (request, response) => {
-  const accountname = String(request.query.id);
-  const body = request.body;
-  const account = books.find((account) => account.id === accountname);
   const index = books.indexOf(account);
 
   if (!account) {
@@ -100,10 +130,10 @@ app.delete("/books/:id", (request, response) => {
     response.status(500).send("Account not found.");
   } else {
     books = newbooks;
-    response.send(trainees);
+    response.send(books);
   }
 });
 
-app.listen(3000, () => {
-  console.log(`Server running on 3000`);
+app.listen(config.appPort, () => {
+  console.log(`Server running on ${config.appPort}`);
 });
